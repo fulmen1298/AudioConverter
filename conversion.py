fa1f712de878f6290b51d85da1fb1e6a85f30c35
Ralph Boilard <fulmen1298@gmail.com>
2023-07-11 07:55:55 -0500

    Removed unneeded code.

--- conversion.py
+++ conversion.py
@@ -10,7 +10,6 @@ def convertWavToMp3():
     t.start()
     totalCount = 0
     failed_files = []
-    all_wav_files = []
     # walk through path and store all child directories in subdirs
     subdirs = os.listdir(path)
     for dirs in subdirs:
@@ -20,7 +19,6 @@ def convertWavToMp3():
         os.chdir(dir_path)
         #grab all .wav and .mp3 files and store them in the corresponding list
         wav_files = glob.glob("./*.wav")
-        all_wav_files = glob.glob("./*.wav")
         mp3_files = glob.glob("./*.mp3")
         print(f"Conversion of .wav files in {dirs} has begun. Please wait for the process to finish.")
         # create mp3 file name check if that mp3 already exists in the directory and skip it if so, if not convert from .wav and export to directory
@@ -28,6 +26,7 @@ def convertWavToMp3():
             mp3_file = os.path.splitext(wav_file)[0] + ".mp3"
             if mp3_file in mp3_files:
                 print("File already converted to mp3.")
+                os.remove(wav_file)
                 continue
             try:
                 sound = pydub.AudioSegment.from_wav(wav_file)
@@ -44,21 +43,6 @@ def convertWavToMp3():
     try:
         print(f"Average time for each file conversion was {elapsed_time / totalCount:04f}.")
         print(f"The following files were unable to be converted {failed_files}")
-        print(f"The following wav files were found {all_wav_files}")
     except:
         print("No files were converted as no .wav files were detected.")
         print(f"The following files were unable to be converted {failed_files}")
-        print(f"The following wav files were found {all_wav_files}")
-    return all_wav_files
-
-def removeWavFiles(wav_files):
-    answer = input(f"Would you like to delete the following files?: {wav_files}").lower()
-    if answer == "y":
-        try:
-            for wav_file in wav_files:
-                os.remove(wav_file)
-                print(f"{wav_file} has been removed.")
-        except:
-            print(f"The following file was unable to be removed {wav_file}.")
-    else:
-        quit()
--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import conversion as c
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    c.removeWavFiles(c.convertWavToMp3())
+    c.convertWavToMp3()
